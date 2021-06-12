3f1705378638a50f3d1b02b02a4786f1e287002b
Riheel Hamoande <riheel83@gmail.com>
2021-06-12 18:44:28 -0500

    switch to cloud

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+require('dotenv').config();
 const compression = require("compression");
 
 const PORT = 3000;
@@ -15,10 +16,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', { 
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
+
 });
+
 
 // routes
 app.use(require("./routes/api.js"));
