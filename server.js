aa1f20977aace0bf55a85403ae8efdec5e15b6c6
Riheel Hamoande <riheel83@gmail.com>
2021-06-12 19:25:52 -0500

    change order of app.use

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-app.use(logger("dev"));
 require('dotenv').config();
 
 const PORT = rocess.env.PORT || 3000;
@@ -12,6 +11,7 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(logger("dev"));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', { 
   useNewUrlParser: true,
